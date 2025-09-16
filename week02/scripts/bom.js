cfdcf2ff2135573d65978b14a020112d44c7a8db
moro118 <mvillalobosortega@byupathway.edu>
2025-09-15 22:31:30 -0600

    update

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,23 @@
-const input = document.querySelector('#favchap');
-const button =document.querySelector('button');
-const list =document.querySelector('#list');
-const li =document.createElement('li');
-const deleteBtn = document.createElement('button');
-li.textContent = input.value;
-deleteBtn.textContent = '❌';
-li.append(deleteBtn);
-list.append(li);
+
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+    const input = document.querySelector('#favchap');
+    const button =document.querySelector('button');
+    const list =document.querySelector('#list');
+    const li =document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    li.textContent = input.value;
+    deleteBtn.textContent = '❌';
+    li.append(deleteBtn);
+    list.append(li);
+        input.value = '';
+        input.focus();
+        deleteBtn.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+    } else {
+        input.focus();
+    }
+});
