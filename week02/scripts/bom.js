62e56e61b5d2445b57a8996aaab4497824a2d47c
moro118 <mvillalobosortega@byupathway.edu>
2025-09-16 10:46:05 -0600

    update bomjs and bom.html

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,16 +1,15 @@
-
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', function() {
     if (input.value.trim() !== '') {
-    const input = document.querySelector('#favchap');
-    const button =document.querySelector('button');
-    const list =document.querySelector('#list');
-    const li =document.createElement('li');
-    const deleteBtn = document.createElement('button');
-    li.textContent = input.value;
-    deleteBtn.textContent = '❌';
-    li.append(deleteBtn);
-    list.append(li);
+        const li = document.createElement('li');
+        const deleteBtn = document.createElement('button');
+        li.textContent = input.value;
+        deleteBtn.textContent = '❌';
+        li.append(deleteBtn);
+        list.append(li);
         input.value = '';
         input.focus();
         deleteBtn.addEventListener('click', function() {
